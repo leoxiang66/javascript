59f888968526dd2f6f210fbcc1061f9099f8bd93
Tao Xiang <46348901+leoxiang66@users.noreply.github.com>
2024-04-14 23:15:53 +0800

    1

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-import { createEmptyMinBST, insert, remove, printTree, getMin } from "functional-data-structure/src/datatype/MinBST.js";
+import { createEmptyMaxBST, insert, remove, printTree, getMax } from "functional-data-structure/src/datatype/MaxBST.js";
 
-var tree = createEmptyMinBST()
-var tree =  insert(tree,10)
-var tree =  insert(tree,1)
-var tree =  insert(tree,-2)
-var tree =  insert(tree,-3)
-var tree =  insert(tree,-10)
-var tree =  insert(tree,10)
-var tree =  insert(tree,20)
-var finalTree =  insert(tree,2)
+var tree = createEmptyMaxBST()
+tree =  insert(tree,10)
+tree =  insert(tree,1)
+tree =  insert(tree,-2)
+tree =  insert(tree,-3)
+tree =  insert(tree,-10)
+tree =  insert(tree,10)
+tree =  insert(tree,20)
+tree =  insert(tree,2)
 
-console.log(getMin(finalTree));
-// printTree(remove(finalTree,-2))
+console.log(getMax(tree));
+
+
+tree = remove(tree,20)
+console.log(getMax(tree))
+
+tree = remove(tree,10)
+console.log(getMax(tree))
+
+tree = remove(tree, 10)
+console.log(getMax(tree))
+
+console.log("print tree:")
+printTree(tree)
